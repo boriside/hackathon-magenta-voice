b9636c5b9a56fa698b684cdb3948f60ef4d8169a
Supreeth Sharma <supreethsharma444@gmail.com>
2020-12-07 22:06:40 +0100

    Updated the skill

--- skill/virtual_teacher.py
+++ skill/virtual_teacher.py
@@ -4,7 +4,15 @@ import logging
 
 
 @skill.intent_handler('TEAM_35_START_VIRTUAL_TEACHER')
-def handler(answer: str):
+def virtual_teacher_handler(answer: str):
+    logger = logging.getLogger(__name__)
+    logger.info("**** Context2 = " + str(context))
+    ask_freetext("What do you want to play")
+
+INTENT_NAME = "CVI_INTERNAL_ASK_FREETEXT"
+
+@skill.intent_handler(INTENT_NAME)
+def handler():
     try:
         logger = logging.getLogger(__name__)
         logger.info("**** Test Context = "+ str(context))
@@ -13,16 +21,7 @@ def handler(answer: str):
             answer = "number"
             print("**** Test Context = "+ str(context))
             response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
-        return ask_freetext(response.text)
+        return ask(response.text)
     except Exception as e:
         logger.info("**** Exception = " + str(e))
         return ask("Sorry. I didn't understand that. Can you please repeat ?")
-
-
-INTENT_NAME = "CVI_INTERNAL_ASK_FREETEXT"
-
-@skill.intent_handler(INTENT_NAME)
-def handler():
-    logger = logging.getLogger(__name__)
-    logger.info("**** Context2 = " + str(context))
-    ask_freetext("What do you want to play")
