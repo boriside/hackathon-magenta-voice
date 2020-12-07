c13f75f4df9c06723defd4553abda3af58522708
Supreeth Sharma <supreethsharma444@gmail.com>
2020-12-07 21:44:51 +0100

    Updted the skill

--- skill/virtual_teacher.py
+++ skill/virtual_teacher.py
@@ -1,11 +1,28 @@
-from skill_sdk import skill, Response, tell
+from skill_sdk import skill, Response, tell, ask, context, ask_freetext
 from skill_sdk.requests import CircuitBreakerSession
+import logging
+
 
 @skill.intent_handler('TEAM_35_START_VIRTUAL_TEACHER')
 def handler(answer: str):
     try:
+        logger = logging.getLogger(__name__)
+        logger.info("**** Test Context = "+ str(context))
         with CircuitBreakerSession() as session:
+
+            answer = "number"
+            print("**** Test Context = "+ str(context))
             response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
-        return tell(response.text)
-    except:
-        return tell("Sorry. I didn't understand that. Can you please repeat ?")
+        return ask_freetext(response.text)
+    except Exception as e:
+        logger.info("**** Exception = " + str(e))
+        return ask("Sorry. I didn't understand that. Can you please repeat ?")
+
+
+INTENT_NAME = "CVI_INTERNAL_ASK_FREETEXT"
+
+@skill.intent_handler(INTENT_NAME)
+def handler():
+    logger = logging.getLogger(__name__)
+    logger.info("**** Context2 = " + str(context))
+    ask_freetext("What do you want to play")
