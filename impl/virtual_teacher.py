61ea3a2a746b2c6909de7ba99b08c5e8965d2c16
boris_ide <boriside@gmail.com>
2020-12-08 15:02:23 +0100

    clean up repo

--- impl/virtual_teacher.py
+++ impl/virtual_teacher.py
@@ -1,15 +1,25 @@
-from skill_sdk import skill, Response, tell
+from skill_sdk import skill, Response, tell, ask, context, ask_freetext
 from skill_sdk.requests import CircuitBreakerSession
+import logging
 
-@skill.intent_handler('VIRTUAL_TEACHER_NUMBER')
-def handler(answer: str):
+
+@skill.intent_handler('TEAM_35_START_VIRTUAL_TEACHER')
+def virtual_teacher_handler(answer: str):
+    logger = logging.getLogger(__name__)
+    logger.info("**** Context2 = " + str(context))
+    return ask_freetext("Was möchtest du heute spielen? Mathe Fragen oder Geometrie?")
+
+@skill.intent_handler('CVI_INTERNAL_ASK_FREETEXT')
+def handler():
     try:
+        logger = logging.getLogger(__name__)
+        logger.info("**** Test Context = "+ str(context))
         with CircuitBreakerSession() as session:
-            response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
-        return tell(response.text)
-    except:
-        return tell("Sorry. I didn't understand that. Can you please repeat ?")
 
-"""
-1) CVI_INTERNAL_ASK_FREETEXT
-"""
+            answer = "Mathe"
+            print("**** Test Context = "+ str(context))
+            response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
+        return ask(response.text)
+    except Exception as e:
+        logger.info("**** Exception = " + str(e))
+        return ask("Wie bitte?")
--- skill/README.md
+++
@@ -1,2 +0,0 @@
-# skill
-Add code for the skill here
