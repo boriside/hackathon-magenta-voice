ea732ca84ba3a4ed2fbaf3c8c8616c3ebe72fb7d
boris_ide <boriside@gmail.com>
2020-12-08 12:52:23 +0100

    fix intent name and use german

--- skill/virtual_teacher.py
+++ skill/virtual_teacher.py
@@ -7,21 +7,19 @@ import logging
 def virtual_teacher_handler(answer: str):
     logger = logging.getLogger(__name__)
     logger.info("**** Context2 = " + str(context))
-    return ask_freetext("What do you want to play")
+    return ask_freetext("Was möchtest du heute spielen?")
 
-INTENT_NAME = "CVI_INTERNAL_ASK_FREETEXT"
-
-@skill.intent_handler(INTENT_NAME)
+@skill.intent_handler('CVI_INTERNAL_ASK_FREETEXT')
 def handler():
     try:
         logger = logging.getLogger(__name__)
         logger.info("**** Test Context = "+ str(context))
         with CircuitBreakerSession() as session:
 
-            answer = "number"
+            answer = "Mathe"
             print("**** Test Context = "+ str(context))
             response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
         return ask(response.text)
     except Exception as e:
         logger.info("**** Exception = " + str(e))
-        return ask("Sorry. I didn't understand that. Can you please repeat ?")
+        return ask("Wie bitte?")
