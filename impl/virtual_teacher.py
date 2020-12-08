fe2d823f12a5ec5abe198ca9502409af75862ccd
boris_ide <boriside@gmail.com>
2020-12-08 17:09:30 +0100

    add stt

--- impl/virtual_teacher.py
+++ impl/virtual_teacher.py
@@ -10,15 +10,15 @@ def virtual_teacher_handler(answer: str):
     return ask_freetext("Was möchtest du heute spielen? Mathe Fragen oder Geometrie?")
 
 @skill.intent_handler('CVI_INTERNAL_ASK_FREETEXT')
-def handler():
+def handler(stt: str):
     try:
         logger = logging.getLogger(__name__)
         logger.info("**** Test Context = "+ str(context))
         with CircuitBreakerSession() as session:
 
-            answer = "Mathe"
+            #answer = "Mathe"
             print("**** Test Context = "+ str(context))
-            response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ answer)
+            response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ stt)
         return ask(response.text)
     except Exception as e:
         logger.info("**** Exception = " + str(e))
