1be98d16eb1bd5dc4add13d3a342b4dfedf67b8e
boris_ide <boriside@gmail.com>
2020-12-09 21:02:10 +0100

    last changes

--- impl/virtual_teacher.py
+++ impl/virtual_teacher.py
@@ -1,5 +1,6 @@
-from skill_sdk import skill, Response, tell, ask, context, ask_freetext
+from skill_sdk import skill, Response, tell, ask, context, ask_freetext, ssml
 from skill_sdk.requests import CircuitBreakerSession
+from skill_sdk.services.persistence import PersistenceService
 import logging
 
 
@@ -7,17 +8,21 @@ import logging
 def virtual_teacher_handler(answer: str):
     logger = logging.getLogger(__name__)
     logger.info("**** Context2 = " + str(context))
-    return ask_freetext("Was möchtest du heute spielen? Mathe Fragen oder Geometrie? Ich kann dir auch einige Fitness Übungen zeigen.")
+    return ask_freetext("Was möchtest du heute machen? Algebra oder Geometrie? Ich kann dir auch einige Fitness Übungen zeigen.")
 
 @skill.intent_handler('CVI_INTERNAL_ASK_FREETEXT')
 def handler(stt: str):
     try:
         logger = logging.getLogger(__name__)
         logger.info("**** Test Context = "+ str(context))
+        #session_id = PersistenceService().get()['session_id']
+        #if not session_id:
+            #PersistenceService().set(dict(session_id=context.session.session_id))
         with CircuitBreakerSession() as session:
             print("**** Test Context = "+ str(context))
             response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=new_id2&answer='+ stt)
-        return ask(response.text)
+            #response = session.get('https://hi4m6llff6.execute-api.eu-west-1.amazonaws.com/prod/a?id=' + session_id +'&answer=' + stt)
+        return ask_freetext(response.text)
     except Exception as e:
         logger.info("**** Exception = " + str(e))
-        return ask("Wie bitte?")
+        return ask("Es ist ein Fehler aufgetreten!")
